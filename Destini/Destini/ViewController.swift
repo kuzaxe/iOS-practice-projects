f98b1559f8a6f9a15735fe0e009ed920bcfbd3cc
Raj <kuzaxe@gmail.com>
2019-03-11 22:31:06 -0400

    Destini Path choosing app

---
+++ Destini/Destini/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Philipp Muellauer on 01/09/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!
+    
+    var state = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: UIControl.State.normal)
+        bottomButton.setTitle(answer1b, for: UIControl.State.normal)
+    }
+
+    
+    // User presses one of the buttons
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if (state == 1) {
+            if (sender.tag == 1) {
+                updateView(story: story3, ans1: answer3a, ans2: answer3b, newState: 3)
+            } else {
+                updateView(story: story2, ans1: answer2a, ans2: answer2b, newState: 2)
+            }
+        } else if (state == 2) {
+            if (sender.tag == 1) {
+                updateView(story: story3, ans1: answer3a, ans2: answer3b, newState: 3)
+            } else {
+                updateView(story: story4, ans1: "", ans2: "", newState: 0)
+            }
+        } else if (state == 3) {
+            if (sender.tag == 1) {
+                updateView(story: story6, ans1: "", ans2: "", newState: 0)
+            } else {
+                updateView(story: story5, ans1: "", ans2: "", newState: 0)
+            }
+        }
+        
+        if (state == 0) {
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+                let alert = UIAlertController(title: "Awesome", message: "Game completed! Do you want to start over?", preferredStyle: .alert)
+                let restartAction = UIAlertAction(title: "Restart", style: .default, handler:
+                { (UIAlertAction) in
+                    self.startOver()
+                })
+                
+                alert.addAction(restartAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func updateView(story: String, ans1: String, ans2: String, newState: Int) {
+        storyTextView.text = story
+        topButton.setTitle(ans1, for: UIControl.State.normal)
+        bottomButton.setTitle(ans2, for: UIControl.State.normal)
+        state = newState
+    }
+    
+    func startOver() {
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: UIControl.State.normal)
+        bottomButton.setTitle(answer1b, for: UIControl.State.normal)
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+        state = 1
+    }
+}
+
